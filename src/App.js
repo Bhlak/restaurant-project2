228bc763dab2c950cf5c79fac50260ab3848e819
Bhlak <bhlakcodes@protonmail.com>
2022-04-22 06:34:40 +0100

    Changes made stated in the telegram group

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Nav/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Products from "./components/Products/Products";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="/contact" element={<Contact />}></Route>
+          <Route exact path="/products" element={<Products />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,5 +1,27 @@
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Brah</div>;
+  return (
+    <nav>
+      <span>E N O R M I C O</span>
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
