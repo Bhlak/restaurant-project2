64d6d54d8dae583fca6a228d95546fcb9a5235d6
Bhlak <bhlakcodes@protonmail.com>
2022-04-27 04:37:06 +0100

    Updates

---
+++ precache-manifest.1b304b3247b3450718d088597360d210.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "42972cd57296b01f4caf",
+    "url": "/restaurant-project2/static/css/main.5ef2de32.chunk.css"
+  },
+  {
+    "revision": "42972cd57296b01f4caf",
+    "url": "/restaurant-project2/static/js/main.42972cd5.chunk.js"
+  },
+  {
+    "revision": "75d9e4df4ae4855d8004",
+    "url": "/restaurant-project2/static/css/1.e1d538ae.chunk.css"
+  },
+  {
+    "revision": "75d9e4df4ae4855d8004",
+    "url": "/restaurant-project2/static/js/1.75d9e4df.chunk.js"
+  },
+  {
+    "revision": "0cf73242274d3be8c3e6",
+    "url": "/restaurant-project2/static/js/runtime~main.0cf73242.js"
+  },
+  {
+    "revision": "59c1a98af2b29381b520bb2d02ed4fb0",
+    "url": "/restaurant-project2/static/media/gold-cutlery-set-black-background.59c1a98a.jpg"
+  },
+  {
+    "revision": "3d002a3d0285d5e3ef6ad8b9269ceb0f",
+    "url": "/restaurant-project2/static/media/soup-restaurant.3d002a3d.jpg"
+  },
+  {
+    "revision": "e1f0c91ad890889fae0e6a8e09d01dd5",
+    "url": "/restaurant-project2/static/media/enormico.e1f0c91a.png"
+  },
+  {
+    "revision": "f701e169cb17dd0a62868e7f7b49f837",
+    "url": "/restaurant-project2/static/media/img1.f701e169.jpg"
+  },
+  {
+    "revision": "3d002a3d0285d5e3ef6ad8b9269ceb0f",
+    "url": "/restaurant-project2/static/media/shrimp-avocado-soup.3d002a3d.jpg"
+  },
+  {
+    "revision": "388ca4229db77ddcd25ff74c46251d1a",
+    "url": "/restaurant-project2/static/media/Baked-stuffed-potatoes.388ca422.jpg"
+  },
+  {
+    "revision": "7b38eb7a106b99664da2060d7b78c46a",
+    "url": "/restaurant-project2/static/media/Beef soup.7b38eb7a.jpg"
+  },
+  {
+    "revision": "b1037de3ea1f4f0959f77fc9ebaf2eca",
+    "url": "/restaurant-project2/static/media/Carrot soup.b1037de3.jpg"
+  },
+  {
+    "revision": "1ac9e2d30fb663c65522e2ce1ee5a338",
+    "url": "/restaurant-project2/static/media/Chips.1ac9e2d3.jpeg"
+  },
+  {
+    "revision": "5269759fe556d0d6e7f3881e9ea48e1d",
+    "url": "/restaurant-project2/static/media/Cocktail.5269759f.jpg"
+  },
+  {
+    "revision": "d16e4c6a78956fdd62a98f35f6143ce7",
+    "url": "/restaurant-project2/static/media/fried-paneer.d16e4c6a.jpg"
+  },
+  {
+    "revision": "16e8ecba22df2dba2a30301df0b0d23d",
+    "url": "/restaurant-project2/static/media/Grilled-cheese.16e8ecba.jpg"
+  },
+  {
+    "revision": "a82f02b42fc9e12af26ec78374edba9a",
+    "url": "/restaurant-project2/static/media/Kabab.a82f02b4.jpg"
+  },
+  {
+    "revision": "970231756bedeecd7e598401130b2a14",
+    "url": "/restaurant-project2/static/media/Tomato soup.97023175.jpg"
+  },
+  {
+    "revision": "38568e98a4f4660599aa3a88147b6c93",
+    "url": "/restaurant-project2/static/media/Veggy soup.38568e98.jpg"
+  },
+  {
+    "revision": "8ef7dd578a91df728f994f30df068a00",
+    "url": "/restaurant-project2/static/media/wilted-spinach.8ef7dd57.jpg"
+  },
+  {
+    "revision": "8ccacd6ce0ef55a322412b438978a4a7",
+    "url": "/restaurant-project2/static/media/victoria-shes-UC0HZdUitWY-unsplash.8ccacd6c.jpg"
+  },
+  {
+    "revision": "0cd9fdcf2355c5b408ba100735e9642e",
+    "url": "/restaurant-project2/static/media/interior.0cd9fdcf.jpg"
+  },
+  {
+    "revision": "76c1440571d08927eea610f9e268ff66",
+    "url": "/restaurant-project2/index.html"
+  }
+];
